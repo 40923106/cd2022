027fc70bac6dcb9ea6baca76a7136c05afb21eb2
40923106 <40923106@gm.nfu.edu.tw>
2022-03-15 21:05:06 +0800

    NX1980 title

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲網址\xa0 https://github.com/40923106/cd2022 \n', 'tags': '', 'url': 'About.html'}, {'title': 'NX1980', 'text': '', 'tags': '', 'url': 'NX1980.html'}]};
